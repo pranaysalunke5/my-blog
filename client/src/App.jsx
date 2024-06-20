0396b6652e36ae5abfb729a26e96fd69109a7cbf
Pranay <pranaysalunke5@gmail.com>
2024-06-20 17:59:12 +0530

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
-import {BrowserRouter,Routes,Route} from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/home" element={<Navigate to="/" />} /> {/* Redirect /home to / */}
+        <Route path="*" element={<Navigate to="/" />} /> {/* Handle unknown routes */}
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
